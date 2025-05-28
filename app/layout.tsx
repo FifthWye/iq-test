a4175d93197670b1aa22347992590b0ed4605a86
5y <41546028+FifthWye@users.noreply.github.com>
2025-05-28 11:33:02 +0200

    misspell fixed :)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'IQ Test Pro',
-  description: 'Try your inelegance',
+  description: 'Try your intelligence',
 }
 
 export default function RootLayout({
