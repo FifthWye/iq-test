7e2fa48ff29f36b7ce1363f5da27855478e708bb
5y <41546028+FifthWye@users.noreply.github.com>
2025-06-27 22:57:43 +0200

    google analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
+import GoogleAnalytics from "../components/google-analytics"
 import "./globals.css"
 
 export const metadata: Metadata = {
@@ -15,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body><GoogleAnalytics />{children}</body>
     </html>
   )
 }
